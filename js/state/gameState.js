0c0fd3df131cf1b687364e54b4d60ef16f8d5728
Krealer <Krealerkimizuki@gmail.com>
2026-03-09 05:45:43 +0000

    Refactor project into modular static-file structure

---
+++ js/state/gameState.js
@@ -0,0 +1,26 @@
+import { DEFAULT_MAP_ID, BATTLE_MENUS } from '../utils/constants.js';
+
+export const gameState = {
+  currentLanguage: 'en',
+  currentSlotId: null,
+  gameMenuStatusKey: '',
+  showCoordinates: false,
+  pendingDeleteSlotId: null,
+  currentMapId: DEFAULT_MAP_ID,
+  enemyStates: [],
+  npcStates: []
+};
+
+export const playerState = {
+  tileX: 0,
+  tileY: 0,
+  path: [],
+  moving: false,
+  stepToX: 0,
+  stepToY: 0,
+  stepElapsed: 0,
+  lastTimestamp: null
+};
+
+export const dialogueState = { npcId: null, nodeId: null };
+export const battleState = { menu: BATTLE_MENUS.ROOT, enemyId: null, enemy: null, player: null, feedbackKeys: [], resultMessageKey: 'obtainedNothing', resultItemKey: null };
---
+++ js/state/settingsState.js
@@ -0,0 +1,1 @@
+export const settingsState = { showCoordinates: false };
